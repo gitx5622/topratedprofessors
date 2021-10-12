0b8c541bc542dfd4b72c9b3a49091bd0bbccf0c7
gitx5622 <ggitau5622@gmail.com>
2021-10-12 14:03:09 +0300

    changed wallet margin to 10px

--- src/components/orders/components/filter-component.js
+++ src/components/orders/components/filter-component.js
@@ -1,12 +1,15 @@
 /** @jsx jsx */
 import React from 'react';
-import {jsx, Box, Input, Button} from 'theme-ui';
+import {jsx, Box, Input} from 'theme-ui';
 import {AiOutlineClose} from 'react-icons/ai';
 import {CSVLink} from "react-csv";
 
 const FilterComponent = ({onFilter, onClear, filterText, data, section}) => {
     return (
-        <Box sx={{ display: 'flex', justifyContent: 'space-around', gap: '2.5em'}}>
+        <Box sx={{ display: 'flex', justifyContent: 'space-between', gap:1, width:'100%'}}>
+            <Box sx={{display: 'flex'}}>
+                <h3>{section.toUpperCase().replace(/_/g, " ")}</h3>
+            </Box>
             <Box sx={{display: 'flex'}}>
             <Input sx={styles.input} value={filterText} onChange={onFilter}   placeholder='Search by Order Number'/>
             <Box sx={{height: '35px', width: '50px', background: '#2979FF', borderTopRightRadius: '5px', borderBottomRightRadius: '5px',}}>
