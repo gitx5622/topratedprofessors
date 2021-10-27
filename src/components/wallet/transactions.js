7bc72b5dc0961cf36a4af676c23d92a0aacbfaf8
gitx5622 <ggitau5622@gmail.com>
2021-10-27 14:27:45 +0300

    paypal integration

--- src/components/wallet/transactions.js
+++ src/components/wallet/transactions.js
@@ -1,23 +1,23 @@
 import React from 'react';
-import {  useDispatch } from "react-redux";
+import { useDispatch } from "react-redux";
 import WalletCard from "./components/wallet-card";
-import {executePayment} from "../../dataStore/actions/walletAtion";
-import {useRouter} from "next/router";
+import { executePayment } from "../../dataStore/actions/walletAtion";
+import { useRouter } from "next/router";
+import { loc } from "globby";
 
-const Transactions = ({section}) => {
+const Transactions = ({ section }) => {
     const dispatch = useDispatch();
     const router = useRouter();
     const payerID = router.query.PayerID;
     const paymentId = router.query.paymentId;
-    console.log(payerID);
-    console.log(paymentId);
 
     React.useEffect(() => {
+        localStorage.PID = payerID;
+        localStorage.Pid = paymentId;
+        window.history.replaceState(null, '', '/dashboard/transactions');
         const { id: userID } = JSON.parse(localStorage.currentUser);
-        if (payerID && paymentId) {
-            executePayment(dispatch, userID, paymentId, payerID).then(response => {
-                if(response.status === 200) router.push('/dashboard/transactions', undefined, {shallow: true});
-            })
+        if (localStorage.PID && localStorage.Pid) {
+            executePayment(dispatch, userID, paymentId, payerID);
         }
     }, [dispatch, payerID, paymentId])
     return (
