7d9fec8610b4bc365675bcadbfe5cc61e06763b7
gitx5622 <ggitau5622@gmail.com>
2021-12-09 17:17:58 +0300

    dashboard improvements

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
 const withPlugins = require('next-compose-plugins');
 const optimizedImages = require('next-optimized-images');
-
+module.exports = {
+    exportTrailingSlash: true
+}
 module.exports = withPlugins([optimizedImages], { target: 'serverless' });
